85c241de03f3ac01f6c62c0a4f0e5d77791a8525
ihpolyphe <ihpolyphe@gmail.com>
2024-11-09 11:39:16 +0900

    add header

--- scripts/scrayping/scrayping_horse_info.py
+++ scripts/scrayping/scrayping_horse_info.py
@@ -13,7 +13,10 @@ assume_id = "202408060411"
 
 
 def get_horse_info(column,assume_url):
-    r=requests.get(assume_url)
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    r=requests.get(assume_url,headers=headers)
     soup = BeautifulSoup(r.content.decode("euc-jp", "ignore"), "html.parser")#バグ対策でdecode
     soup_span = soup.find_all("span")
     allnum=(len(soup_span)-6)/3#馬の数

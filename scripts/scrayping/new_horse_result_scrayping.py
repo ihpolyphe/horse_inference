1dffc5e6bab16edc4be354126120817fbeeb065e
ihpolyphe <ihpolyphe@gmail.com>
2025-01-21 22:41:22 +0900

    update

--- scripts/scrayping/new_horse_result_scrayping.py
+++ scripts/scrayping/new_horse_result_scrayping.py
@@ -7,6 +7,7 @@ import random
 import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import sys
 
 
 #============レースの結果をスクレイピングするクラス============
@@ -157,6 +158,9 @@ class HorseResults:
                 df = pd.read_html(html.text)[2]
                 df.index = [horse_id] * len(df)
                 horse_results[horse_id] = df
+                # horse_idも保存する。
+                horse_results[horse_id]["horse_id"] = horse_id
+
             except IndexError:
                 continue
             except Exception as e:
@@ -223,6 +227,9 @@ class Peds:
                     df = df.drop_duplicates()
                 ped = pd.concat([generations[i] for i in range(5)]).rename(horse_id)
 
+                # horse_idも保存する。
+                ped["horse_id"] = horse_id
+
                 peds_dict[horse_id] = ped.reset_index(drop=True)
             except IndexError:
                 continue
@@ -237,32 +244,118 @@ class Peds:
 
         return peds_df
     
+class jockeyResults:
+    @staticmethod
+    def scrape(jockey_id_list):
+        """
+        騎手の過去成績データをスクレイピングする関数
+
+        Parameters:
+        ----------
+        jockey_id_list : list
+            騎手IDのリスト
+
+        Returns:
+        ----------
+        jockey_results_df : pandas.DataFrame
+            全騎手の過去成績データをまとめてDataFrame型にしたもの
+        """
+        USER_AGENTS = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:115.0) Gecko/20100101 Firefox/115.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.0.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/85.0.4341.72",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/85.0.4341.72",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Vivaldi/5.3.2679.55",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Vivaldi/5.3.2679.55",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Brave/1.40.107",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Brave/1.40.107",
+        ]
+        jockey_results = {}
+        for jockey_id in tqdm(jockey_id_list):
+            time.sleep(1)
+            try:
+                url = 'https://db.netkeiba.com/jockey/' + jockey_id
+                headers = {'User-Agent': random.choice(USER_AGENTS),
+                           'Referer': "https://db.netkeiba.com/",
+                           'Accept-Language': 'ja-jp,ja;q=0.9,en-US;q=0.8,en;q=0.7'
+                           }
+                html = requests.get(url, headers=headers)
+                html.encoding = "EUC-JP"
+                df = pd.read_html(html.text)[2]
+                df.index = [jockey_id] * len(df)
+                jockey_results[jockey_id] = df
+                # horse_idも保存する。
+                jockey_results[jockey_id]["jockey_id"] = jockey_id
+
+            except IndexError:
+                continue
+            except Exception as e:
+                print(e)
+                break
+            except:
+                break
+
+
 #============スクレイピング実行============
 race_id_list = []
-scrayping_year = "2022"
+scrayping_year = "2025"
 for place in range(1, 11, 1):
+# for place in range(1, 2, 1):
     for kai in range(1, 7, 1):
+    # for kai in range(1, 2, 1):
         for day in range(1, 13, 1):
+        # for day in range(1, 2, 1):
             for r in range(1, 13, 1):
+            # for r in range(1, 2, 1):
                 race_id = scrayping_year + str(place).zfill(2) + str(kai).zfill(2) + str(day).zfill(2) + str(r).zfill(2)
                 race_id_list.append(race_id)
 
 # 取得したrace resultsをcsvに保存
-is_denso = True
+is_denso = False
+is_windows = True
 if is_denso:
+    if is_windows:
+        sys.exit()
     save_path = "/home/denso/horse_inference/data_for_train/train_data"
 else:
-    save_path = "/home/hayato/horse_inference/data_for_train/train_data/"
+    if not is_windows:
+        save_path = "/home/hayato/horse_inference/data_for_train/train_data/"
+    else:
+        save_path = r"C:\Users\hayat\Desktop\horse_inference\scripts\scrayping"
+
+print(save_path)
 # 保存先のディレクトリが存在しない場合は作成
 if not os.path.exists(save_path):
     os.makedirs(save_path)
-# レース結果のスクレイピング
-results = Results.scrape(race_id_list)
-results.to_csv(save_path+"_results_" + str(scrayping_year) + ".csv", index=False)
+
+# is_saveがTrueだった場合、csvのデータからhorse_id_listを取得
+is_save = False
+if is_save:
+    results = pd.read_csv(save_path+"train_data_results_" + str(scrayping_year) + ".csv")
+
+else:
+    # レース結果のスクレイピング
+    print("start horse race scrayping")
+    results = Results.scrape(race_id_list)
+    results.to_csv(save_path+"train_data_results_" + str(scrayping_year) + ".csv", index=False)
+
+
 # 馬の過去成績のスクレイピング
 horse_id_list = results['horse_id'].unique()
+jockey_id_list = results['jockey_id'].unique()
+print("start horse result race scrayping")
 horse_results = HorseResults.scrape(horse_id_list)
+print(horse_results)
 horse_results.to_csv(save_path+"horse_results_" + str(scrayping_year) + ".csv", index=False)
 # 馬の血統データのスクレイピング
-horse_scrape_results = HorseResults.scrape(horse_id_list)
+print("start Peds scrayping")
+horse_scrape_results = Peds.scrape(horse_id_list)
 horse_scrape_results.to_csv(save_path+"horse_born_results_" + str(scrayping_year) + ".csv", index=False)
+# jokeyの過去成績のスクレイピング
+print("start jokey scrayping")
+jockey_results = jockeyResults.scrape(jockey_id_list)
+jockey_results.to_csv(save_path+"jockey_results_" + str(scrayping_year) + ".csv", index=False)
